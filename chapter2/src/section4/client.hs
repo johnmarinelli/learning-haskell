be846a3fb770a04fd1c546943830140831c8e4dd
John Marinelli <jonmarinelli@hotmail.com>
2015-03-31 17:42:31 -0700

    adts

---
+++ chapter2/src/section4/client.hs
@@ -0,0 +1,12 @@
+-- client algebraic data type
+-- different ctors are used to represent completely different alternatives
+
+data Client = GovOrg     String -- name
+            | Company    String Integer Person -- name, id, contact, contact position
+            | Individual Person Bool -- name, last name, receive offers
+            deriving Show -- let haskell override toString automatically
+                          -- Show is a class
+
+--   type     ctor   constructor args
+data Person = Person String String 
+              deriving Show
