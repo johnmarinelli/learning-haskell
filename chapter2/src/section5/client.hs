5033539bbe2db8f7f2cb13c865f2520c7a1a689c
John Marinelli <jonmarinelli@hotmail.com>
2015-04-02 13:14:32 -0700

    countgenders

--- chapter2/src/section5/client.hs
+++ chapter2/src/section5/client.hs
@@ -2,9 +2,11 @@ data Client = GovOrg String
             | Company String Integer Person Bool
             | Individual Person Bool
             deriving Show
-data Person = Person String String
+data Person = Person String String Gender
             deriving Show
 
+data Gender = Male | Female | Unknown
+            deriving (Show, Eq)
 {-
 -- pattern matching:
 clientName :: Client -> String
@@ -24,7 +26,7 @@ clientName client = case client of
 -- Encode the pattern directly in definition of function
 clientName (GovOrg name)                       = name
 clientName (Company name _ _ _)                = name
-clientName (Individual (Person fname lname) _) = fname ++ lname
+clientName (Individual (Person fname lname _) _) = fname ++ lname
 
 -- 'partial' functions; not defined over complete domain of their argument
 -- Maybe is (hilariously) a type that is parameterized by the value it holds
@@ -36,4 +38,45 @@ companyName client = case client of
                     Company name _ _ _ -> Just name
                     -- or Nothing.  which is nothing
                     _                  -> Nothing
+
+-- returns the gender of client.
+clientGender :: Client -> Maybe Gender
+clientGender client = case client of
+                      Company _ _ (Person _ _ gender)  _ -> Just gender
+                      Individual (Person _ _ gender) _   -> Just gender
+                      _                                  -> Nothing
+
+-- just an exercise for count genders
+countBools :: [Bool] -> Integer
+countBools lst = if null lst
+                 then 0
+                 else
+                   if True == head lst
+                   then 1 + countBools (tail lst)
+                   else 0 + countBools (tail lst)
+
+-- for accessing triples of Integer
+first :: (Integer, Integer, Integer) -> Integer
+first (x, _, _) = x
+
+second :: (Integer, Integer, Integer) -> Integer
+second (_, x, _) = x
+
+third :: (Integer, Integer, Integer) -> Integer
+third (_, _, x)  = x
+
+-- counts gender of a list of clients
+countGenders :: [Client] -> (Integer, Integer, Integer)
+countGenders clients = if null clients
+                       then (0, 0, 0)
+                       else
+                         -- foreach (client in clients)
+                         let client = head clients
+                         in if (clientGender client) == Just Female
+                            then (1 + (first genders), 0 + (second genders), 0 + (third genders))
+                            else if (clientGender client) == Just Male
+                            then (0 + (first genders), 1 + (second genders), 0 + (third genders))
+                            else (0 + (first genders), 0 + (second genders), 1 + (third genders))
+                            -- recursive call
+                            where genders = countGenders (tail clients)
 
